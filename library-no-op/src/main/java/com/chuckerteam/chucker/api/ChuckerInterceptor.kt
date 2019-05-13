403d42edeac825a23d57fa1f0d5f5bf55a19d40b
Nicola Corti <corti.nico@gmail.com>
2019-05-12 19:26:49 -0700

    Kotlinize the Public API of Chucker
    I converted to Kotlin all the public exposed classes of Chucker.
    I also converted to kotlin the `no-op` package to make it easier to
    maintain once we do further api changes.
    
    IMPORTANT: Please note that this change introduce some breaking changes
    and requires a major version bump.
    
    Summary of Changes:
    - Converted all the classes in `com.chuckerteam.chucker.api` to Kotlin
    - The `no-op` module now is all in Kotlin and uses the Kotlin android
    plugin
    - Refactored the historical builder pattern of Interceptor/Collector to
    use named paramters instead
    - Moved the `PERIOD` enum to be inside the `RetentionManager`
    - Updated visibility of multiple methods to be `internal`

---
+++ library-no-op/src/main/java/com/chuckerteam/chucker/api/ChuckerInterceptor.kt
@@ -0,0 +1,27 @@
+package com.chuckerteam.chucker.api
+
+import android.content.Context
+import okhttp3.Interceptor
+import okhttp3.Response
+import java.io.IOException
+
+/**
+ * No-op implementation.
+ */
+class ChuckerInterceptor @JvmOverloads constructor(
+        context: Context?,
+        collector: Any? = null,
+        maxContentLength: Any? = null,
+        headersToRedact: Any? = null
+) : Interceptor {
+
+    fun redactHeaders(vararg names: String): ChuckerInterceptor {
+        return this
+    }
+
+    @Throws(IOException::class)
+    override fun intercept(chain: Interceptor.Chain): Response {
+        val request = chain.request()
+        return chain.proceed(request)
+    }
+}
---
+++ library/src/main/java/com/chuckerteam/chucker/api/RetentionManager.kt
@@ -0,0 +1,101 @@
+package com.chuckerteam.chucker.api
+
+import android.content.Context
+import android.content.SharedPreferences
+import android.util.Log
+import com.chuckerteam.chucker.api.Chucker.LOG_TAG
+
+import com.chuckerteam.chucker.api.internal.data.repository.RepositoryProvider
+
+import java.util.concurrent.TimeUnit
+
+/**
+ * Class responsible of holding the logic for the retention of your HTTP transactions
+ * and your throwable. You can customize how long data should be stored here.
+ * @param context An Android Context
+ * @param retentionPeriod A [Period] to specify the retention of data. Default 1 week.
+ */
+class RetentionManager @JvmOverloads constructor(
+        context: Context,
+        retentionPeriod: Period = Period.ONE_WEEK
+) {
+
+    // The actual retention period in milliseconds (default to
+    private val period: Long
+    // How often the cleanup should happen
+    private val cleanupFrequency: Long
+    private val prefs: SharedPreferences
+
+    init {
+        period = toMillis(retentionPeriod)
+        prefs = context.getSharedPreferences(PREFS_NAME, 0)
+
+        cleanupFrequency = if (retentionPeriod == Period.ONE_HOUR)
+            TimeUnit.MINUTES.toMillis(30)
+        else
+            TimeUnit.HOURS.toMillis(2)
+    }
+
+    /**
+     * Call this function to check and eventually trigger a cleanup.
+     * Please note that this method is not forcing a cleanup.
+     */
+    @Synchronized
+    internal fun doMaintenance() {
+        if (period > 0) {
+            val now = System.currentTimeMillis()
+            if (isCleanupDue(now)) {
+                Log.i(LOG_TAG, "Performing data retention maintenance...")
+                deleteSince(getThreshold(now))
+                updateLastCleanup(now)
+            }
+        }
+    }
+
+    private fun getLastCleanup(fallback: Long): Long {
+        if (lastCleanup == 0L) {
+            lastCleanup = prefs.getLong(KEY_LAST_CLEANUP, fallback)
+        }
+        return lastCleanup
+    }
+
+    private fun updateLastCleanup(time: Long) {
+        lastCleanup = time
+        prefs.edit().putLong(KEY_LAST_CLEANUP, time).apply()
+    }
+
+    private fun deleteSince(threshold: Long) {
+        RepositoryProvider.transaction().deleteOldTransactions(threshold)
+        RepositoryProvider.throwable().deleteOldThrowables(threshold)
+    }
+
+    private fun isCleanupDue(now: Long) = now - getLastCleanup(now) > cleanupFrequency
+
+    private fun getThreshold(now: Long) = if (period == 0L) now else now - period
+
+    private fun toMillis(period: Period): Long {
+        return when (period) {
+            Period.ONE_HOUR -> TimeUnit.HOURS.toMillis(1)
+            Period.ONE_DAY -> TimeUnit.DAYS.toMillis(1)
+            Period.ONE_WEEK -> TimeUnit.DAYS.toMillis(7)
+            Period.FOREVER -> 0
+        }
+    }
+
+    enum class Period {
+        /** Retain data for the last hour. */
+        ONE_HOUR,
+        /** Retain data for the last day. */
+        ONE_DAY,
+        /** Retain data for the last week. */
+        ONE_WEEK,
+        /** Retain data forever. */
+        FOREVER
+    }
+
+    companion object {
+        private const val PREFS_NAME = "chucker_preferences"
+        private const val KEY_LAST_CLEANUP = "last_cleanup"
+        private var lastCleanup: Long = 0
+    }
+}
