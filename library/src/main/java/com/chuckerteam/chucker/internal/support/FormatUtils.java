919254653f5abc3c521b24fd18ad649f0fb0e372
Nicola Corti <corti.nico@gmail.com>
2019-09-05 10:50:39 +0200

    Kotlinify JsonConverter
    I've also renamed the class from JsonConvertor to JsonConverter
    and added some JUnit tests.

--- library/src/main/java/com/chuckerteam/chucker/internal/support/FormatUtils.java
+++ library/src/main/java/com/chuckerteam/chucker/internal/support/FormatUtils.java
@@ -51,7 +51,7 @@ public class FormatUtils {
         try {
             JsonParser jp = new JsonParser();
             JsonElement je = jp.parse(json);
-            return JsonConvertor.getInstance().toJson(je);
+            return JsonConverter.getInstance().toJson(je);
         } catch (Exception e) {
             return json;
         }
