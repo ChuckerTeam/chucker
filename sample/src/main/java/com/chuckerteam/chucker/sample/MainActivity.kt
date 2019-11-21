f7fd151ba0ecde39a14d75ab92022247ce53637e
Anoop S S <anoopvvs@gmail.com>
2019-11-21 14:25:23 +0530

    Sample updated to have config to use localized date format

--- sample/src/main/java/com/chuckerteam/chucker/sample/MainActivity.kt
+++ sample/src/main/java/com/chuckerteam/chucker/sample/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.chuckerteam.chucker.api.Chucker
+import com.chuckerteam.chucker.api.Configs
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -14,6 +15,10 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         client = HttpBinClient(applicationContext)
+
+        Chucker.configs = Configs.Builder()
+                .localisedTime(true)
+                .build()
 
         do_http.setOnClickListener { client.doHttpActivity() }
         trigger_exception.setOnClickListener { client.recordException() }
