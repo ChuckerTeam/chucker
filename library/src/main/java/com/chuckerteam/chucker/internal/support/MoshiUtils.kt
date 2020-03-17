08ac994b67959052aed49db0d1591e0d789d3488
Hitanshu Dhawan <hitanshudhawan1996@gmail.com>
2020-03-17 18:37:29 +0530

    changed variable name to jsonAdapter

--- library/src/main/java/com/chuckerteam/chucker/internal/support/MoshiUtils.kt
+++ library/src/main/java/com/chuckerteam/chucker/internal/support/MoshiUtils.kt
@@ -7,8 +7,8 @@ import com.squareup.moshi.Types
 
 internal fun Moshi.getHttpHeaderListJsonAdapter(): JsonAdapter<List<HttpHeader>> {
     val type = Types.newParameterizedType(List::class.java, HttpHeader::class.java)
-    val adapter = this.adapter<List<HttpHeader>>(type)
-    return adapter.addConvenienceMethods()
+    val jsonAdapter = this.adapter<List<HttpHeader>>(type)
+    return jsonAdapter.addConvenienceMethods()
 }
 
 internal fun <T> JsonAdapter<T>.addConvenienceMethods(): JsonAdapter<T> {
