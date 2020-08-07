a6c3c56b93046dfdd2de7a437cbd610b06517a5e
Jay Newstrom <jaynewstrom@gmail.com>
2020-08-07 04:44:48 -0500

    Fix FileProvider issues. (#410)
    This prevents conflicts with other apps that already use the generic androidx.core.content.FileProvider as a name.
    This also allows multiple apps with chucker to be installed on a device at one time.
    
    Fixes #409

---
+++ library/src/main/java/com/chuckerteam/chucker/internal/support/ChuckerFileProvider.kt
@@ -0,0 +1,9 @@
+package com.chuckerteam.chucker.internal.support
+
+import androidx.core.content.FileProvider
+
+/**
+ * We need our own subclass so we don't conflict with other [FileProvider]s
+ * See: https://github.com/ChuckerTeam/chucker/issues/409
+ */
+class ChuckerFileProvider : FileProvider()
