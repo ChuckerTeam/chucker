73d472914bf39364a8eaefb7b7a7be33ecb4e5a6
Michał Sikora <michalsikora90@gmail.com>
2020-08-24 15:45:12 +0200

    Add KDoc for CacheDirectoryProvider

--- library/src/main/java/com/chuckerteam/chucker/internal/support/CacheDirectoryProvider.kt
+++ library/src/main/java/com/chuckerteam/chucker/internal/support/CacheDirectoryProvider.kt
@@ -2,6 +2,10 @@ package com.chuckerteam.chucker.internal.support
 
 import java.io.File
 
+/**
+ * An interface that returns a reference to a cache directory where temporary files can be
+ * saved.
+ */
 internal fun interface CacheDirectoryProvider {
     fun provide(): File?
 }
