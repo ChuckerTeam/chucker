96cc1c09da84c2f0a87d0c7a4e89594b913f3dcf
Israel <israel.nascimento@b2wdigital.com>
2021-07-24 17:18:19 -0300

    Fix crash

--- library/src/main/java/com/chuckerteam/chucker/internal/support/GroupSingleton.kt
+++ library/src/main/java/com/chuckerteam/chucker/internal/support/GroupSingleton.kt
@@ -3,18 +3,5 @@ package com.chuckerteam.chucker.internal.support
 import com.chuckerteam.chucker.api.Group
 
 internal object GroupSingleton {
-    internal lateinit var groups: MutableList<Group>
-
-    fun getMergedGroups(others: List<Group>): List<Group> {
-        others.forEach { other ->
-            val group = groups.find { it.id == other.id }!!
-            groups.updateItem(group, other)
-        }
-        return groups
-    }
-
-    private fun <T> MutableList<T>.updateItem(item: T, newItem: T) {
-        val index = this.indexOf(item)
-        this[index] = newItem
-    }
+    internal var groups: MutableList<Group> = mutableListOf()
 }
