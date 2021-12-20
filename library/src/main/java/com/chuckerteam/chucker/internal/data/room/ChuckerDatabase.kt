6cc621167994b3462a515bd9c512fe042cd83e64
Volodymyr Buberenko <v.buberenko@gmail.com>
2021-12-20 20:40:06 +0200

    Remove cleanup for old Chuck DB file

--- library/src/main/java/com/chuckerteam/chucker/internal/data/room/ChuckerDatabase.kt
+++ library/src/main/java/com/chuckerteam/chucker/internal/data/room/ChuckerDatabase.kt
@@ -12,13 +12,9 @@ internal abstract class ChuckerDatabase : RoomDatabase() {
     abstract fun transactionDao(): HttpTransactionDao
 
     companion object {
-        private const val OLD_DB_NAME = "chuck.db"
         private const val DB_NAME = "chucker.db"
 
         fun create(applicationContext: Context): ChuckerDatabase {
-            // We eventually delete the old DB if a previous version of Chuck/Chucker was used.
-            applicationContext.getDatabasePath(OLD_DB_NAME).delete()
-
             return Room.databaseBuilder(applicationContext, ChuckerDatabase::class.java, DB_NAME)
                 .fallbackToDestructiveMigration()
                 .build()
