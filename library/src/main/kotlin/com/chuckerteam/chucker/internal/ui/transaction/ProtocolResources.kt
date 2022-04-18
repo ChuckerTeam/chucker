74536e5a47169c023825addc07c22c0787a8339c
Althaf <althafkbacker@gmail.com>
2022-04-18 13:04:09 +0530

    GQL introduced to ProtocolResources

--- library/src/main/kotlin/com/chuckerteam/chucker/internal/ui/transaction/ProtocolResources.kt
+++ library/src/main/kotlin/com/chuckerteam/chucker/internal/ui/transaction/ProtocolResources.kt
@@ -7,4 +7,5 @@ import com.chuckerteam.chucker.R
 internal sealed class ProtocolResources(@DrawableRes val icon: Int, @ColorRes val color: Int) {
     class Http : ProtocolResources(R.drawable.chucker_ic_http, R.color.chucker_color_error)
     class Https : ProtocolResources(R.drawable.chucker_ic_https, R.color.chucker_color_primary)
+    class Gql : ProtocolResources(R.drawable.chucker_ic_graphql, R.color.chucker_color_primary)
 }
