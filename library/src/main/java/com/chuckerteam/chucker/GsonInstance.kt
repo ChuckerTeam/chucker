f3a3b2a2e1bdf11d9a4f4d3853d001c99a692165
sandeep <sandeep.insan2000@gmail.com>
2022-06-17 10:03:08 +0530

    Add publish script

--- library/src/main/java/com/chuckerteam/chucker/GsonInstance.kt
+++ library/src/main/java/com/chuckerteam/chucker/GsonInstance.kt
@@ -3,7 +3,6 @@ package com.chuckerteam.chucker
 import com.google.gson.Gson
 import com.google.gson.GsonBuilder
 
-
 public object GsonInstance {
     private var gson: Gson? = null
     public fun get(): Gson? {
