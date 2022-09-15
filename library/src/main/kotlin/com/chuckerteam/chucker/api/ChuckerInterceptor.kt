61bad35f57752fff5e157ace67677575a5fc1a0f
Arjan Mundy <amundy@yelp.com>
2022-09-14 23:31:12 -0400

    restores ChuckerInterceptor.kt

--- library/src/main/kotlin/com/chuckerteam/chucker/api/ChuckerInterceptor.kt
+++ library/src/main/kotlin/com/chuckerteam/chucker/api/ChuckerInterceptor.kt
@@ -95,6 +95,7 @@ public class ChuckerInterceptor private constructor(
         internal var headersToRedact = emptySet<String>()
         internal var decoders = emptyList<BodyDecoder>()
         internal var createShortcut = true
+
         /**
          * Sets the [ChuckerCollector] to customize data retention.
          */
