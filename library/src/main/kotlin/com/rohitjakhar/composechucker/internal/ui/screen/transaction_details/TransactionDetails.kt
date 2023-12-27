d894d46bac509bcb81c964d34079206bdc72a927
Rohit Jakhar <rohitjakhar940@gmail.com>
2023-12-28 01:38:12 +0530

    Fix string resource and dialog.

--- library/src/main/kotlin/com/rohitjakhar/composechucker/internal/ui/screen/transaction_details/TransactionDetails.kt
+++ library/src/main/kotlin/com/rohitjakhar/composechucker/internal/ui/screen/transaction_details/TransactionDetails.kt
@@ -3,7 +3,11 @@ package com.rohitjakhar.composechucker.internal.ui.screen.transaction_details
 import android.content.Context
 import android.text.SpannableStringBuilder
 import android.util.Log
+import android.widget.Toast
+import androidx.compose.animation.AnimatedContent
+import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
@@ -11,7 +15,14 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.rememberPagerState
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Delete
+import androidx.compose.material.icons.rounded.Search
+import androidx.compose.material.icons.rounded.Share
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Tab
 import androidx.compose.material3.TabRow
@@ -20,12 +31,17 @@ import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.core.text.HtmlCompat
 import androidx.lifecycle.asFlow
 import androidx.lifecycle.viewmodel.compose.viewModel
@@ -56,19 +72,37 @@ internal fun TransactionDetails(
     val payloadItems = remember {
         mutableStateListOf<TransactionPayloadItem>()
     }
+    var showDeleteDialog by remember {
+        mutableStateOf(false)
+    }
+    var showShareDialog by remember {
+        mutableStateOf(false)
+    }
+
     LaunchedEffect(key1 = horizontalState.currentPage, block = {
         Log.d("testrohit", "TransactionDetails: current: ${horizontalState.currentPage}")
         Log.d("testrohit", "TransactionDetails: target: ${horizontalState.targetPage}")
         when (horizontalState.currentPage) {
             1 -> {
-                val items = processPayload(type = PayloadType.REQUEST, transactionsData.value, true, context)
+                val items = processPayload(
+                    type = PayloadType.REQUEST,
+                    transactionsData.value,
+                    true,
+                    context
+                )
                 payloadItems.clear()
                 payloadItems.addAll(
                     items
                 )
             }
+
             2 -> {
-                val items = processPayload(type = PayloadType.RESPONSE, transactionsData.value, true, context)
+                val items = processPayload(
+                    type = PayloadType.RESPONSE,
+                    transactionsData.value,
+                    true,
+                    context
+                )
                 payloadItems.clear()
                 payloadItems.addAll(
                     items
@@ -85,23 +119,102 @@ internal fun TransactionDetails(
     Scaffold(
         topBar = {
             TopAppBar(title = {
-                Text(text = titles[horizontalState.currentPage])
+                AnimatedContent(targetState = horizontalState.currentPage, label = "") {
+                    Text(text = titles[it])
+                }
+            }, actions = {
+                Icon(
+                    imageVector = Icons.Rounded.Search,
+                    contentDescription = "",
+                    modifier = Modifier
+                        .padding(horizontal = 4.dp)
+                        .clickable {
+                            Toast
+                                .makeText(context, "search clicked", Toast.LENGTH_SHORT)
+                                .show()
+                        }
+                )
+                Icon(
+                    imageVector = Icons.Rounded.Share,
+                    contentDescription = "share",
+                    modifier = Modifier
+                        .padding(horizontal = 4.dp)
+                        .clickable {
+                            showShareDialog = true
+                        }
+                )
+                Icon(
+                    imageVector = Icons.Rounded.Delete,
+                    contentDescription = "delete",
+                    modifier = Modifier
+                        .padding(horizontal = 4.dp)
+                        .clickable {
+                            showDeleteDialog = true
+                        }
+                )
             })
         }
     ) { paddingValues ->
+        AnimatedVisibility(visible = showDeleteDialog) {
+            AlertDialog(
+                onDismissRequest = {
+                    showDeleteDialog = false
+                },
+                text = {
+                    Text(text = stringResource(id = R.string.chucker_clear_http_transaction_confirmation))
+                },
+                confirmButton = {
+                    Text(text = stringResource(id = R.string.chucker_yes),
+                        Modifier.clickable {
+                            showDeleteDialog = false
+                        })
+                },
+                dismissButton = {
+                    Text(text = stringResource(id = R.string.chucker_cancel), Modifier.clickable {
+                        showDeleteDialog = false
+                    })
+                }
+            )
+        }
+        AnimatedVisibility(visible = showShareDialog) {
+            AlertDialog(
+                onDismissRequest = {
+                    showShareDialog = false
+                },
+                text = {
+                    Text(text = context.getString(R.string.chucker_share_as_curl))
+                },
+                confirmButton = {
+                    Text(text = stringResource(id = R.string.chucker_yes), Modifier.clickable {
+                        showShareDialog = false
+                    })
+                },
+                dismissButton = {
+                    Text(text = stringResource(id = R.string.chucker_cancel), Modifier.clickable {
+                        showShareDialog = false
+                    })
+                }
+            )
+        }
         Column(
             modifier = Modifier
                 .padding(paddingValues)
                 .fillMaxSize()
         ) {
-            TabRow(selectedTabIndex = horizontalState.currentPage) {
+            TabRow(selectedTabIndex = horizontalState.currentPage,
+                divider = {
+                    Divider()
+                }) {
                 titles.forEachIndexed { index, s ->
-                    Tab(selected = index == horizontalState.currentPage, onClick = {
-                        scope.launch {
-                            horizontalState.animateScrollToPage(index)
-                        }
-                    }) {
-                        Text(text = s)
+                    Tab(
+                        selected = index == horizontalState.currentPage,
+                        onClick = {
+                            scope.launch {
+                                horizontalState.animateScrollToPage(index)
+                            }
+                        },
+                    ) {
+                        Text(text = s, modifier = Modifier.padding(12.dp))
                     }
                 }
             }
@@ -111,12 +224,13 @@ internal fun TransactionDetails(
             ) { page: Int ->
                 when (page) {
                     1, 2 -> {
-                        LazyColumn (modifier = Modifier.fillMaxSize()) {
+                        LazyColumn(modifier = Modifier.fillMaxSize()) {
                             items(payloadItems) {
                                 TransactionPayloadBody(transactionPayloadItem = it)
                             }
                         }
                     }
+
                     else -> {
                         TransactionOverview(
                             httpTransaction = transactionsData.value,
