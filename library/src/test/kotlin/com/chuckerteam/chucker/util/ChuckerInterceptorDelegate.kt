33e81da59f609ae9836dac0459b0b9da70193f98
VenomVendor <1086927+VenomVendor@users.noreply.github.com>
2024-07-09 22:41:51 -0400

    Added tests to evauluate path and domain ignore rules
    Added requried mocks for okHttp

--- library/src/test/kotlin/com/chuckerteam/chucker/util/ChuckerInterceptorDelegate.kt
+++ library/src/test/kotlin/com/chuckerteam/chucker/util/ChuckerInterceptorDelegate.kt
@@ -21,6 +21,9 @@ internal class ChuckerInterceptorDelegate(
     cacheDirectoryProvider: CacheDirectoryProvider,
     decoders: List<BodyDecoder> = emptyList(),
     skipPaths: List<String> = emptyList(),
+    skipPathsRegex: List<Regex> = emptyList(),
+    skipDomains: List<String> = emptyList(),
+    skipDomainsRegex: List<Regex> = emptyList(),
 ) : Interceptor {
     private val idGenerator = AtomicLong()
     private val transactions = CopyOnWriteArrayList<HttpTransaction>()
@@ -47,6 +50,9 @@ internal class ChuckerInterceptorDelegate(
             .alwaysReadResponseBody(alwaysReadResponseBody)
             .cacheDirectorProvider(cacheDirectoryProvider)
             .skipPaths(skipPaths = skipPaths.toTypedArray())
+            .skipDomains(skipDomains = skipDomains.toTypedArray())
+            .apply { skipPathsRegex.forEach(::skipPaths) }
+            .apply { skipDomainsRegex.forEach(::skipDomains) }
             .apply { decoders.forEach(::addBodyDecoder) }
             .build()
 
