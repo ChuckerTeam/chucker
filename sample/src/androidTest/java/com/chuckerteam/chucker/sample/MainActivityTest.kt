7d39be783d1a06b1d434dd5223215efa6a7f6bef
codedbyaman <amankumarbostan@gmail.com>
2025-04-30 23:07:30 +0530

    added some more test cases and added string class for string utils

--- sample/src/androidTest/java/com/chuckerteam/chucker/sample/MainActivityTest.kt
+++ sample/src/androidTest/java/com/chuckerteam/chucker/sample/MainActivityTest.kt
@@ -1,42 +1,86 @@
+package com.chuckerteam.chucker.sample
+
 import androidx.test.core.app.ActivityScenario
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.isChecked
 import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.ext.junit.runners.AndroidJUnit4
-import com.chuckerteam.chucker.sample.MainActivity
+import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
 class MainActivityTest {
+    @Before
+    fun setup() {
+        ActivityScenario.launch(MainActivity::class.java)
+    }
+
     @Test
     fun testUIComponentsAreDisplayed() {
-        ActivityScenario.launch(MainActivity::class.java)
-
-        // Interceptor type label
-        onView(withText("Interceptor type:")).check(matches(isDisplayed()))
-
-        // Application radio button
-        onView(withText("Application")).check(matches(isDisplayed()))
-        onView(withText("Application")).perform(click())
-
-        // Network radio button
-        onView(withText("Network")).check(matches(isDisplayed()))
-        onView(withText("Network")).perform(click())
-
-        // Buttons
-        onView(withText("DO HTTP ACTIVITY")).check(matches(isDisplayed()))
-        onView(withText("DO GRAPHQL ACTIVITY")).check(matches(isDisplayed()))
-        onView(withText("LAUNCH CHUCKER DIRECTLY")).check(matches(isDisplayed()))
-        onView(withText("EXPORT TO LOG FILE")).check(matches(isDisplayed()))
-        onView(withText("EXPORT TO HAR FILE")).check(matches(isDisplayed()))
+        onView(withText(ChuckerUITestLabels.INTERCEPTOR_TYPE_LABEL)).check(matches(isDisplayed()))
+        onView(withText(ChuckerUITestLabels.APPLICATION_RADIO_BUTTON)).check(matches(isDisplayed()))
+        onView(withText(ChuckerUITestLabels.APPLICATION_RADIO_BUTTON)).perform(click())
+        onView(withText(ChuckerUITestLabels.NETWORK_RADIO_BUTTON)).check(matches(isDisplayed()))
+        onView(withText(ChuckerUITestLabels.NETWORK_RADIO_BUTTON)).perform(click())
+        onView(withText(ChuckerUITestLabels.DO_HTTP_ACTIVITY_BUTTON)).check(matches(isDisplayed()))
+        onView(withText(ChuckerUITestLabels.DO_GRAPHQL_ACTIVITY_BUTTON)).check(matches(isDisplayed()))
+        onView(withText(ChuckerUITestLabels.LAUNCH_CHUCKER_BUTTON)).check(matches(isDisplayed()))
+        onView(withText(ChuckerUITestLabels.EXPORT_LOG_FILE_BUTTON)).check(matches(isDisplayed()))
+        onView(withText(ChuckerUITestLabels.EXPORT_HAR_FILE_BUTTON)).check(matches(isDisplayed()))
     }
 
     @Test
     fun testButtonInteractions() {
-        ActivityScenario.launch(MainActivity::class.java)
-        onView(withText("DO HTTP ACTIVITY")).perform(click())
+        onView(withText(ChuckerUITestLabels.DO_HTTP_ACTIVITY_BUTTON)).perform(click())
+    }
+
+    @Test
+    fun testRadioButtonSelection() {
+        onView(withText(ChuckerUITestLabels.APPLICATION_RADIO_BUTTON))
+            .perform(click())
+            .check(matches(isChecked()))
+
+        onView(withText(ChuckerUITestLabels.NETWORK_RADIO_BUTTON))
+            .perform(click())
+            .check(matches(isChecked()))
+    }
+
+    @Test
+    fun testHttpActivityButtonClick() {
+        onView(withText(ChuckerUITestLabels.DO_HTTP_ACTIVITY_BUTTON)).perform(click())
+    }
+
+    @Test
+    fun testGraphQLActivityButtonClick() {
+        onView(withText(ChuckerUITestLabels.DO_GRAPHQL_ACTIVITY_BUTTON)).perform(click())
+    }
+
+    @Test
+    fun testLaunchChuckerDirectlyButtonClick() {
+        onView(withText(ChuckerUITestLabels.LAUNCH_CHUCKER_BUTTON)).perform(click())
+    }
+
+    @Test
+    fun testExportButtonsVisibility() {
+        onView(withText(ChuckerUITestLabels.EXPORT_LOG_FILE_BUTTON)).check(matches(isDisplayed()))
+        onView(withText(ChuckerUITestLabels.EXPORT_HAR_FILE_BUTTON)).check(matches(isDisplayed()))
+    }
+
+    @Test
+    fun testAllButtonClicks() {
+        val buttons =
+            listOf(
+                ChuckerUITestLabels.DO_HTTP_ACTIVITY_BUTTON,
+                ChuckerUITestLabels.DO_GRAPHQL_ACTIVITY_BUTTON,
+                ChuckerUITestLabels.LAUNCH_CHUCKER_BUTTON,
+            )
+
+        buttons.forEach { buttonText ->
+            onView(withText(buttonText)).perform(click())
+        }
     }
 }
