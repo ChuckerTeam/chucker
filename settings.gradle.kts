005936a9bccd07dc201cc6b866c0ac21e0892592
Shivanand Yadav <shivanandyadav12a@gmail.com>
2025-06-18 22:28:46 +0530

    Initial commit to migrate to version catalog

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,21 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
 include(":sample", ":library", ":library-no-op")
